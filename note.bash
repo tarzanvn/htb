6e4ab13ad78b447fe65f61c432161d78446a1d3a
TarzanVN <81658035+tarzanvn@users.noreply.github.com>
2022-12-06 01:41:22 +0700

    add Netcat Shell Stabilisation

--- note.bash
+++ note.bash
@@ -23,3 +23,13 @@
 # subdomain enum
 	# fuzz
 	ffuf -w /usr/share/wordlists/SecLists/Discovery/DNS/namelist.txt -H "Host: FUZZ.acmeitsupport.thm" -u http://10.10.184.72 -fs {size}
+	
+	
+# Netcat Shell Stabilisation (ref: https://tryhackme.com/room/introtoshells)
+	## python shell
+	python -c "import pty;pty.spawn('/bin/bash');"
+	export TERM=xterm
+	
+	#ctrl + z: to run process in background.
+	# enable tab, and ctrl+c; then return the process.
+	stty raw -echo; fg
